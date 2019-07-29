70acb43e47211e80fd4ee5e0ad10752f107ad7b5
vishaldad <vishal.dadhwal@basware.com>
2019-07-29 10:03:00 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i am going to build on jenkins!!!");
+        }
+    }
+}
